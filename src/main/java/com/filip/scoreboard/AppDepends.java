710c0c1d245c5ab52f1b4a7f83f22bc5aedf4bf5
filipopo <filipmymail@gmail.com>
2024-11-07 20:25:06 +0100

    Add getter methods

--- src/main/java/com/filip/scoreboard/AppDepends.java
+++ src/main/java/com/filip/scoreboard/AppDepends.java
@@ -12,8 +12,16 @@ class Player {
     this.score = score;
   }
 
-  public int index;
-  public int score;
+  private int index;
+  private int score;
+
+  public int getIndex() {
+    return index;
+  }
+
+  public int getScore() {
+    return score;
+  }
 }
 
 class Team {
@@ -21,36 +29,57 @@ class Team {
     this.index = index;
   }
 
-  public int index;
-  public ArrayList<Player> player = new ArrayList<Player>();
+  private int index;
+  private ArrayList<Player> player = new ArrayList<Player>();
+
+  public int getIndex() {
+    return index;
+  }
+
+  public ArrayList<Player> getPlayer() {
+    return player;
+  }
 
   public int score() {
     int sum = 0;
     for(Player p : player)
-      sum += p.score;
+      sum += p.getScore();
 
     return sum;
   }
 }
 
 class TeamManager {
-  ArrayList<Team> team = new ArrayList<Team>();
-  ArrayList<Player> player = new ArrayList<Player>();
+  private ArrayList<Team> team = new ArrayList<Team>();
+  private ArrayList<Player> player = new ArrayList<Player>();
+
+  public ArrayList<Team> getTeam() {
+    return team;
+  }
 
   public void addTeam() {
     team.add(new Team(team.size() + 1));
   }
 
+  public ArrayList<Player> getPlayer() {
+    return player;
+  }
+
   public void addPlayer(int i, int score) {
     Player p = new Player(player.size() + 1, score);
-    team.get(i).player.add(p);
+    Team t = team.get(i);
+
+    t.getPlayer().add(p);
     player.add(p);
   }
 
   public void sortTeams() {
     for (int i = 0; i < team.size() - 1; i++) {
       for (int j = 0; j < team.size() - i - 1; j++) {
-        if (team.get(j).score() < team.get(j + 1).score()) {
+        int score = team.get(j).score();
+        int nextScore = team.get(j + 1).score();
+
+        if (score < nextScore) {
           Team t = team.get(j);
           team.set(j, team.get(j + 1));
           team.set(j + 1, t);
@@ -62,7 +91,10 @@ class TeamManager {
   public void sortPlayers() {
     for (int i = 0; i < player.size() - 1; i++) {
       for (int j = 0; j < player.size() - i - 1; j++) {
-        if (player.get(j).score < player.get(j + 1).score) {
+        int score = player.get(j).getScore();
+        int nextScore = player.get(j + 1).getScore();
+
+        if (score < nextScore) {
           Player p = player.get(j);
           player.set(j, player.get(j + 1));
           player.set(j + 1, p);

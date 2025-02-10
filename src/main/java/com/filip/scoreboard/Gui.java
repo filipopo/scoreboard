75c22042f00066ca9fca4be12358ddd1c34cdbf4
filipopo <filipmymail@gmail.com>
2025-02-10 15:36:07 +0100

    Remove unused imports

--- src/main/java/com/filip/scoreboard/Gui.java
+++ src/main/java/com/filip/scoreboard/Gui.java
@@ -2,8 +2,6 @@ package com.filip.scoreboard;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
